5c7e126554f49af2ff306f778fb939367f2a4b30
Jack Egan <jackegan248@gmail.com>
2019-11-10 22:09:05 +0000

    Reading URLS and Extracting FORMS
    Retrieving HTML from URL
    Extracting FORM Strings

---
+++ Form Extractor/Form Extractor/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Form_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblURL = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtConsole = new System.Windows.Forms.RichTextBox();
+            this.lblConsole = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(74, 48);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(57, 13);
+            this.lblURL.TabIndex = 0;
+            this.lblURL.Text = "Enter URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(137, 45);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(509, 20);
+            this.txtURL.TabIndex = 1;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(12, 166);
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.Size = new System.Drawing.Size(776, 485);
+            this.txtConsole.TabIndex = 2;
+            this.txtConsole.Text = "";
+            // 
+            // lblConsole
+            // 
+            this.lblConsole.AutoSize = true;
+            this.lblConsole.Location = new System.Drawing.Point(9, 150);
+            this.lblConsole.Name = "lblConsole";
+            this.lblConsole.Size = new System.Drawing.Size(48, 13);
+            this.lblConsole.TabIndex = 3;
+            this.lblConsole.Text = "Console:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(652, 43);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 663);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblConsole);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lblURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.RichTextBox txtConsole;
+        private System.Windows.Forms.Label lblConsole;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Form Extractor/Form Extractor/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string URL = txtURL.Text;
+            string HTML = getHTML(URL);
+
+            List<string> forms = ExtractForm(HTML);
+
+            foreach (var item in forms)
+            {
+                txtConsole.Text += "\n" + item;
+                txtConsole.Text += "\n\n\n========================================================\n\n";
+            }
+        }
+
+        private List<string> ExtractForm(String HTML) {
+            List<string> forms = new List<string>();
+            int startpoint = 0;
+
+            while (startpoint < HTML.Length)
+            {
+                int start = HTML.IndexOf("<form");
+                int end = HTML.IndexOf("</form>");
+                if (start == end)
+                {
+                    break;
+                }
+
+                string data = HTML.Substring(start, (end - start + 7));
+
+                forms.Add(data);
+
+                HTML = HTML.Substring(end + 7, HTML.Length - (end + 7) - 1);
+                startpoint = end + 7;
+            }
+
+            return forms;
+        }
+
+        private string getHTML(string URL)
+        {
+            string result;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage response = client.GetAsync(URL).Result)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            result = content.ReadAsStringAsync().Result;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtConsole.Text += "\n\n" + ex.ToString();
+                throw;
+            }
+            
+            return result;
+        }
+    }
+}
